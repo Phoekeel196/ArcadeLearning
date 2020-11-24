26ac9bb4904b0c9eb5ab77ebac52bab35c81b20b
Phoenix Keeling <phoekeel196@outlook.com>
2020-11-24 14:23:50 -0500

    Add project files.

---
+++ ArcadeLearning/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace ArcadeLearning
+{
+    partial class A
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Edge1 = new System.Windows.Forms.Label();
+            this.Edge2 = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Label();
+            this.Number3 = new System.Windows.Forms.Label();
+            this.Number2 = new System.Windows.Forms.Label();
+            this.Number1 = new System.Windows.Forms.Label();
+            this.label25 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Edge1
+            // 
+            this.Edge1.BackColor = System.Drawing.Color.White;
+            this.Edge1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Edge1.Location = new System.Drawing.Point(226, 9);
+            this.Edge1.Name = "Edge1";
+            this.Edge1.Size = new System.Drawing.Size(350, 450);
+            this.Edge1.TabIndex = 0;
+            // 
+            // Edge2
+            // 
+            this.Edge2.Location = new System.Drawing.Point(239, 18);
+            this.Edge2.Name = "Edge2";
+            this.Edge2.Size = new System.Drawing.Size(325, 425);
+            this.Edge2.TabIndex = 1;
+            // 
+            // Start
+            // 
+            this.Start.Font = new System.Drawing.Font("Millenium BdEx BT", 38F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.ForeColor = System.Drawing.Color.White;
+            this.Start.Location = new System.Drawing.Point(267, 33);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(270, 87);
+            this.Start.TabIndex = 2;
+            this.Start.Text = "Start";
+            this.Start.Click += new System.EventHandler(this.Start_Click_2);
+            // 
+            // Number3
+            // 
+            this.Number3.Font = new System.Drawing.Font("Millenium BdEx BT", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number3.ForeColor = System.Drawing.Color.Black;
+            this.Number3.Location = new System.Drawing.Point(341, 99);
+            this.Number3.Name = "Number3";
+            this.Number3.Size = new System.Drawing.Size(100, 105);
+            this.Number3.TabIndex = 3;
+            this.Number3.Text = "3";
+            // 
+            // Number2
+            // 
+            this.Number2.Font = new System.Drawing.Font("Millenium BdEx BT", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number2.ForeColor = System.Drawing.Color.Black;
+            this.Number2.Location = new System.Drawing.Point(341, 204);
+            this.Number2.Name = "Number2";
+            this.Number2.Size = new System.Drawing.Size(100, 105);
+            this.Number2.TabIndex = 4;
+            this.Number2.Text = "2";
+            // 
+            // Number1
+            // 
+            this.Number1.Font = new System.Drawing.Font("Millenium BdEx BT", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number1.ForeColor = System.Drawing.Color.Black;
+            this.Number1.Location = new System.Drawing.Point(359, 309);
+            this.Number1.Name = "Number1";
+            this.Number1.Size = new System.Drawing.Size(100, 105);
+            this.Number1.TabIndex = 5;
+            this.Number1.Text = "1";
+            // 
+            // label25
+            // 
+            this.label25.Font = new System.Drawing.Font("Microsoft Uighur", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label25.ForeColor = System.Drawing.Color.White;
+            this.label25.Location = new System.Drawing.Point(582, 327);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(256, 145);
+            this.label25.TabIndex = 6;
+            this.label25.Text = "25 C";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(0, 442);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 40);
+            this.label1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(0, 402);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 40);
+            this.label2.TabIndex = 8;
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Purple;
+            this.label3.Location = new System.Drawing.Point(120, 362);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(40, 120);
+            this.label3.TabIndex = 9;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Purple;
+            this.label4.Location = new System.Drawing.Point(80, 402);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 40);
+            this.label4.TabIndex = 10;
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.label5.AllowDrop = true;
+            this.label5.BackColor = System.Drawing.Color.OrangeRed;
+            this.label5.Location = new System.Drawing.Point(40, 322);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 120);
+            this.label5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.OrangeRed;
+            this.label6.Location = new System.Drawing.Point(0, 322);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 40);
+            this.label6.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Blue;
+            this.label7.Location = new System.Drawing.Point(160, 322);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 160);
+            this.label7.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(120, 59);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 80);
+            this.label8.TabIndex = 14;
+            this.label8.Click += new System.EventHandler(this.Label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(160, 20);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 80);
+            this.label9.TabIndex = 15;
+            this.label9.Click += new System.EventHandler(this.Label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.Blue;
+            this.label10.Location = new System.Drawing.Point(82, 242);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 160);
+            this.label10.TabIndex = 16;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.Cyan;
+            this.label11.Location = new System.Drawing.Point(0, 124);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(80, 80);
+            this.label11.TabIndex = 17;
+            // 
+            // label12
+            // 
+            this.label12.BackColor = System.Drawing.Color.Purple;
+            this.label12.Location = new System.Drawing.Point(585, 20);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(220, 80);
+            this.label12.TabIndex = 19;
+            this.label12.Click += new System.EventHandler(this.Label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.BackColor = System.Drawing.Color.Purple;
+            this.label13.Location = new System.Drawing.Point(655, 100);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(80, 80);
+            this.label13.TabIndex = 20;
+            // 
+            // label14
+            // 
+            this.label14.BackColor = System.Drawing.Color.Purple;
+            this.label14.Font = new System.Drawing.Font("mono 07_55", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label14.Location = new System.Drawing.Point(593, 20);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(212, 63);
+            this.label14.TabIndex = 21;
+            this.label14.Text = "TETRIS";
+            // 
+            // A
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(814, 481);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label25);
+            this.Controls.Add(this.Number1);
+            this.Controls.Add(this.Number2);
+            this.Controls.Add(this.Number3);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Edge2);
+            this.Controls.Add(this.Edge1);
+            this.Name = "A";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Edge1;
+        private System.Windows.Forms.Label Edge2;
+        private System.Windows.Forms.Label Start;
+        private System.Windows.Forms.Label Number3;
+        private System.Windows.Forms.Label Number2;
+        private System.Windows.Forms.Label Number1;
+        private System.Windows.Forms.Label label25;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ ArcadeLearning/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace ArcadeLearning
+{
+    public partial class A : Form
+    {
+        public A()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Start_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Start_Click_2(object sender, EventArgs e)
+        {
+            Refresh();
+            Thread.Sleep(500);
+            Start.ForeColor = Color.Black;   
+
+            Refresh();
+            Thread.Sleep(500);
+            Number3.ForeColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(750);
+            Number3.ForeColor = Color.Black;
+
+            Refresh();
+            Thread.Sleep(500);
+            Number2.ForeColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(750);
+            Number2.ForeColor = Color.Black;
+
+            Refresh();
+            Thread.Sleep(500);
+            Number1.ForeColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(500);
+            Number1.ForeColor = Color.Black;
+
+            Refresh();
+            Thread.Sleep(500);
+            Start.ForeColor = Color.Green;
+            Start.BackColor = Color.Green;
+            Edge1.BackColor = Color.Green;
+            Edge2.BackColor = Color.Green;
+            Number3.BackColor = Color.Green;
+            Number3.ForeColor = Color.Green;
+            Number2.BackColor = Color.Green;
+            Number2.ForeColor = Color.Green;
+            Number1.ForeColor = Color.Green;
+            Number1.BackColor = Color.Green;
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label12_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
